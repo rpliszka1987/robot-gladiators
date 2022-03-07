b58e13f84298c1b1d2b444e8786f189778c23a7d
Robert Pliszka <robplisz@gmail.com>
2022-03-07 17:09:11 -0500

    Added JS functions and code to JS file

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,17 @@
-window.alert("This is an alert! JavaScript is running!");
+// this creates a function name "fight"
+
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+var playerName = window.prompt("What is you robo's name?");
+console.log(playerName);
+
+console.log("This logs a string, good for leaving yourself a message");
+
+// This will do math and log 20
+console.log(10+10);
+
+console.log("Our robot's name is " + playerName);
+
+// fight();
